28b40f401e06c029e455224e362573556dfa69ee
JT Olio <hello@jtolio.com>
2022-05-12 00:46:44 -0600

    use multiple implementations concurrently

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.18
 
 require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1
+	github.com/ethereum/go-ethereum v1.10.17
 	golang.org/x/crypto v0.0.0-20220511200225-c6db032c6c88
 )
 
-require golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.1.2 // indirect
+	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
+)
--- keygen/keygen.go
+++ keygen/keygen.go
@@ -7,8 +7,11 @@ import (
 	"fmt"
 	"io"
 	"math/big"
+	"strings"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/ethereum/go-ethereum/crypto"
+	csecp256k1 "github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"golang.org/x/crypto/sha3"
 
 	"github.com/jtolio/dicekeys/entropy"
@@ -17,19 +20,17 @@ import (
 const BytesForEthKey = 40
 
 func DeterministicEthereumKey(source *entropy.Entropy) (private, public string, err error) {
-	// the ethereum curve
-	curve := secp256k1.S256()
+	// we're going to use two separate implementations of ScalarBaseMult on
+	// ethereum's curve to make sure the output is correct.
+	curve1 := secp256k1.S256()
+	params1 := curve1.Params()
+	curve2 := csecp256k1.S256()
+	params2 := curve2.Params()
 
-	// we generate the key manually. we want perfect certainty that the data in
-	// the entropy source is always used in exactly the same way to generate the
-	// key, and the standard library's crypto/ecdsa code has some TODOs right
-	// near the part where the randomness is read. I'd hate for a refactor to
-	// break this.
-
-	// the following code was refactored from the crypto/elliptic package
-	// from the standard library
-	params := curve.Params()
-	buf := make([]byte, params.BitSize/8+8)
+	if params1.BitSize != params2.BitSize {
+		return "", "", fmt.Errorf("differing curve params")
+	}
+	buf := make([]byte, params1.BitSize/8+8)
 	if len(buf) != BytesForEthKey {
 		return "", "", fmt.Errorf("unexpected curve bitsize")
 	}
@@ -38,6 +39,43 @@ func DeterministicEthereumKey(source *entropy.Entropy) (private, public string, 
 		return "", "", err
 	}
 
+	priv1, pub1, err := deterministicEthereumKey(curve1, params1, buf)
+	if err != nil {
+		return "", "", err
+	}
+	priv2, pub2, err := deterministicEthereumKey(curve2, params2, buf)
+	if err != nil {
+		return "", "", err
+	}
+
+	if priv1 != priv2 {
+		return "", "", fmt.Errorf("differing results")
+	}
+	if pub1 != pub2 {
+		return "", "", fmt.Errorf("differing results")
+	}
+
+	// make sure we haven't drifted from geth
+	gethKey, err := crypto.HexToECDSA(priv1)
+	if err != nil {
+		return "", "", err
+	}
+	if pub1 != strings.ToLower(crypto.PubkeyToAddress(gethKey.PublicKey).Hex()) {
+		return "", "", err
+	}
+
+	return priv1, pub1, nil
+}
+
+func deterministicEthereumKey(curve elliptic.Curve, params *elliptic.CurveParams, buf []byte) (private, public string, err error) {
+	// we generate the key manually. we want perfect certainty that the data in
+	// the entropy source is always used in exactly the same way to generate the
+	// key, and the standard library's crypto/ecdsa code has some TODOs right
+	// near the part where the randomness is read. I'd hate for a refactor to
+	// break this.
+
+	// the following code was refactored from the crypto/elliptic package
+	// from the standard library
 	one := new(big.Int).SetInt64(1)
 	k := new(big.Int).SetBytes(buf)
 	n := new(big.Int).Sub(params.N, one)
